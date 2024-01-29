3bc7ce6aead40bc164ecefcdc152537c1aa12142
FRAGt4g <90790095+FRAGt4g@users.noreply.github.com>
2024-01-29 00:35:41 +0000

    Took a long break to watch youtube lmaoooo. Got some stuff done tho

--- Application-Page/Application.js
+++ Application-Page/Application.js
@@ -1,78 +1,32 @@
-var test = document.getElementById("NIHAO");
-var form = document.getElementById("myForm");
-var submitButton = document.getElementById("submitButton");
-var dropdownButton = document.getElementById("positionSelector");
-var dropdownItems = document.querySelectorAll(".dropdown-item");
-var position = document.getElementById("position");
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycby4PhZjkcBe1Bggj0AmYosHU2qPHjnwWIid5wYcPNyBcKVchBHCBEuSnApEBArqAtd7mQ/exec";
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyPadtNTEdWM-sVHtKBoeSLv0Tkz_9PsUZrA2i40IEPVpa7jaGKhaTSN63gNKQRbSl38Q/exec'
+const form = document.forms['submit-to-google-sheet']
+const date = new Date();
 
-//testing ok leave me alone
-/*test.addEventListener("click", function() {
-    if (test.innerHTML == "NI HAO") {
-        test.innerHTML = "IM SPECIAL";
-    } else {
-        test.innerHTML = "NI HAO";
-    }
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  alert(getDate())
+  data = new FormData(form)
+  data.append("Position", document.getElementById("positionSelector").value)
+  data.append("Date", getDate())
+  for (let entry of data.entries()) {
+    console.log(entry[0] + ": " + entry[1]);
+  } 
+  console.log("");
+  fetch(scriptURL, { method: 'POST', body: data})
+    .then(hooray())
+    .catch(error => console.error('Error!', error.message))
+})
 
-});*/
+function hooray() {
+  alert("success");
+}
 
-/*submitButton.addEventListener("click", function() {
-    test.innerHTML = "NOOOOOOOOO";
-    document.getElementById("loadingIcon").classList.remove("d-none");
-})*/
-
-/*form.addEventListener("submit", e => {
-    e.preventDefault()
-    console.log('SCRIPT_URL:', SCRIPT_URL);
-
-    fetch(SCRIPT_URL, { method: 'POST', body: new FormData(form) })
-        .then(response => response.json())
-        .then(data => {
-          console.log('Server response:', data);
-          position.textContent = "Form Has Been Updated!";
-        })
-        .catch(error => {
-          console.error('Error submitting form:', error);
-        });
-
-})*/
-
-form.addEventListener("submit", function (e) {
-    e.preventDefault();
-
-    /*var selectedPosition = dropdownButton.getAttribute("data-position");
-    var formData = new FormData(this);
-    formData.set("position", selectedPosition);*/
-    var selectedPosition = dropdownButton.getAttribute("value");
-    alert(selectedPosition);
-    var formData = new FormData(this);
-    formData.set("position", selectedPosition);
-
-    fetch(SCRIPT_URL, { method: 'POST', body: new FormData(this) })
-      .then(response => response.json())
-      .then(data => {
-        console.log('Server response:', data);
-        position.textContent = "Form Has Been Updated!";
-
-        // Handle success
-        if (data.result === 'success') {
-          alert('Form has been submitted successfully. Updated row: ' + data.row);
-        } else {
-          alert('Form submission failed.');
-        }
-        document.getElementById("loadingIcon").classList.add("d-none");
-      })
-      .catch(error => {
-        console.error('Error submitting form:', error);
-        document.getElementById("loadingIcon").classList.add("d-none");
-      });
-  });
-
-
-// Assuming the URL is something like: https://www.example.com/page?param1=value1&param2=value2
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-
-const job_input = urlParams.get('job');
-const job_dropdown = document.getElementById("positionSelector")
-job_dropdown.value = job_input;
+function getDate() {
+  let month = date.getMonth() + 1;
+  month = month > 10 ? month : "0" + month;
+  
+  let day = date.getDate();
+  day = day > 10 ? day : "0" + day;
+  
+  return date.getFullYear() + "/" + month + "/" + day;
+}
